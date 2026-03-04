37c647afead102f0bebf95c1c92674c78dc17b88
Jon Margree <jonmargree@hotmail.com>
2026-03-04 22:48:34 +0000

    debug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 export default {
   async fetch(request) {
-    return new Response("FuelPilot Worker is running");
+    return new Response("FuelPilot Worker is running", {
+      headers: {
+        "content-type": "text/plain; charset=utf-8",
+        "x-fp-worker": "seo-stations-test"
+      }
+    });
   }
 };
